1ded8d664720e09973bf9de37119acf51d82a876
Francesc <elskater98@gmail.com>
2020-03-05 18:10:34 +0100

    Job:Done

---
+++ deleteDB.sql
@@ -0,0 +1,6 @@
+DELETE FROM eaccessible.accessibilitat;
+DELETE FROM eaccessible.caracteristica;
+DELETE FROM eaccessible.caracteristictipolocal;
+DELETE FROM eaccessible.local;
+DELETE FROM eaccessible.nivell;
+DELETE FROM eaccessible.tipolocal;
