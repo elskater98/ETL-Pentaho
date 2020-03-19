ee9f144a448b44ebff00b0a19e158d3440bd88e2
Francesc <elskater98@gmail.com>
2020-03-19 23:49:06 +0100

    Sort local with nomTipuslocalCa, solve local table

--- deleteDB.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM eaccessible.accessibilitat;
-DELETE FROM eaccessible.caracteristica;
-DELETE FROM eaccessible.caracteristictipolocal;
-DELETE FROM eaccessible.local;
-DELETE FROM eaccessible.nivell;
-DELETE FROM eaccessible.tipolocal;
